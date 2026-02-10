f03c3a6a8006fa1c5bc68b4409f1e906b4fa0c15
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-02-10 23:09:26 +0000

    revision task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,18 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@main
-struct SwiftPlayground {
-    static func main() {
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var day:Int = 1
-        for price in lunches {
-            print("Day \(day): $\(price)")
-            day += 1
-        }
-        print(totalCost(prices: lunches))
-    }
-}
 
 func totalCost(prices: [Double]) -> Double {
     var totalCost: Double = 0.0
@@ -25,3 +13,23 @@ func totalCost(prices: [Double]) -> Double {
 func isOverBudget(total: Double, budget: Double) -> Bool{
     return total > budget
 }  
+
+func averageCost(prices: [Double]) -> Double {
+    return totalCost(prices: prices) / Double(prices.count)
+}
+
+@main
+struct SwiftPlayground {
+    static func main() {
+        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+        var day:Int = 1
+        for price in lunches {
+            print("Day \(day): $\(price)")
+            day += 1
+        }
+        print(totalCost(prices: lunches))
+        print(averageCost(prices: lunches))
+
+        let budget: Double = 35.00
+    }
+}
