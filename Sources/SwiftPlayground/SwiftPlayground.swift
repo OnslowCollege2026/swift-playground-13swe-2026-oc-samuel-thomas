38fb2599888c3ac2c7eae40a015fbb8f90011a7e
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-02-15 01:03:14 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,6 +6,8 @@ struct SwiftPlayground {
     static func main() {
         let passingScore = 50
         let scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
+
+        // curve the scores by adding 5 points to each score, then filter out the scores that are below the passing score, and finally calculate the average of the curved scores
         let curved = scores.map { $0 + 5}
         .filter {$0 >= passingScore}
         let average = curved.reduce(0) { $0 + $1 } / curved.count
