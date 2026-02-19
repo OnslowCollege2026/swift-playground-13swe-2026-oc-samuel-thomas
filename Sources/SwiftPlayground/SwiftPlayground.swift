516698d937d7dbf133bc76378f890080248907fd
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-02-19 21:57:28 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,72 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+struct Student {
+    let id: Int
+    var name: String
+    var age: Int
+    let nsn: Int
+    var email: String
 
-func totalCost(prices: [Double]) -> Double {
-    var totalCost: Double = 0.0
-    for price in prices {
-        totalCost += price
-        }
-    return totalCost
-}
-
-func isOverBudget(total: Double, budget: Double) -> Bool{
-    return total > budget
-}  
-
-func averageCost(prices: [Double]) -> Double {
-    return totalCost(prices: prices) / Double(prices.count)
+    func summary() -> String {
+        return """
+            ID: \(id)
+            Name: \(name)
+            Age: \(age)
+            NSN: \(nsn)
+            Email: \(email)
+            """
+    }
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var day:Int = 1
-        for price in lunches {
-            print("Day \(day): $\(price)")
-            day += 1
-        }
-        print(totalCost(prices: lunches))
-        print(averageCost(prices: lunches))
-        if isOverBudget(total: totalCost(prices: lunches), budget: 35.00) {
-            print("You are over budget!")
-        } else {
-            print("You are within budget.")
-        }
+        let students = [
+            Student(
+                id: 12345,
+                name: "Idris",
+                age: 18,
+                nsn: 123_456_789,
+                email: "idris.elba@student.onslow.school.nz"
+            ),
+            Student(
+                id: 54321,
+                name: "Channing",
+                age: 17,
+                nsn: 987_654_321,
+                email: "channing.tatum@onslow.school.nz"
+            ),
+            Student(
+                id: 54123,
+                name: "Hugh",
+                age: 16,
+                nsn: 321_654_987,
+                email: "hugh.jackman@student.onslow.school.nz"
+            ),
+            Student(
+                id: 32145,
+                name: "Jennifer",
+                age: 18,
+                nsn: 213_546_879,
+                email: "jennifer.lawrence@student.onslow.school.nz"
+            ),
+            Student(
+                id: 45213,
+                name: "Britney",
+                age: 15,
+                nsn: 789_645_312,
+                email: "britney.spears@student.onslow.school.nz"
+            ),
+        ]
+
+        let sam = Student(
+            id: 22361,
+            name: "Sam",
+            age: 16,
+            nsn: 0_144_467_007,
+            email: "samuel.thomas@student.onslow.school.nz"
+        )
+        print(sam.summary())
     }
 }
