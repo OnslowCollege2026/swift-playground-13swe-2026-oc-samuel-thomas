71761dfde756d48250ba055e4847f647bc188a49
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-03-02 02:02:45 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-
-
-func totalCost(prices: [Double]) -> Double {
-    var totalCost: Double = 0.0
-    for price in prices {
-        totalCost += price
-        }
-    return totalCost
-}
-
-func isOverBudget(total: Double, budget: Double) -> Bool{
-    return total > budget
-}  
-
-func averageCost(prices: [Double]) -> Double {
-    return totalCost(prices: prices) / Double(prices.count)
-}
+import Foundation
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var day:Int = 1
-        for price in lunches {
-            print("Day \(day): $\(price)")
-            day += 1
+        struct book {
+            var title: String
+            var author: String 
+            var pages: Int
+
+            func summary() -> String {
+                return "\(title) by \(author) has \(pages) pages."
+            }
+
         }
-        print(totalCost(prices: lunches))
-        print(averageCost(prices: lunches))
-        if isOverBudget(total: totalCost(prices: lunches), budget: 35.00) {
-            print("You are over budget!")
-        } else {
-            print("You are within budget.")
+
+        func bookSummary(title: String, author: String, pages: Int) -> String {
+            return "\(title) by \(author) has \(pages) pages."
         }
+
+        let book1 = book(title: "Blood Meridian", author: "Cormac McCarthy", pages: 337)
+        let book2 = book(title: "Harry Potter and the half-blood prince", author: "J.K. Rowling", pages: 652)
+        print(book1.summary())
+        print(book2.summary())
     }
 }
