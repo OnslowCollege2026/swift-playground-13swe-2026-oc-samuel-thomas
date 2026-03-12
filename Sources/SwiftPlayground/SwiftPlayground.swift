dd59381818e5bcc5b8c0fdf3831d48d0d6980109
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-03-12 22:11:43 +0000

    database

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,6 +3,27 @@
 
 import Foundation
 import GRDB
+
+/// A reservation at the cafe, made by a purchaser
+struct Purchaser: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    /// the Purchaser ID
+    let id: Int
+    /// The name of the purchaser
+    var name: String
+    /// the number of people in the purchaser's party (default 1)
+    var count: Int
+    /// The table reserved by the purchaser
+    var reservedTable: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "Purchaser ID"
+        case name = "Name"
+        case count = "Count"
+        case reservedTable = "Reserved Table"
+    } 
+}
+
+
 @main
 struct SwiftPlayground {
     static func main() {
@@ -10,6 +31,9 @@ struct SwiftPlayground {
         do {
             let dbQueue = try DatabaseQueue(path: dbpath)
             print("database connectoin succesful")
+
+            try dbQueue.read { database in
+            try database.dumpSchema()}
         } catch {
             print(error)
         }
