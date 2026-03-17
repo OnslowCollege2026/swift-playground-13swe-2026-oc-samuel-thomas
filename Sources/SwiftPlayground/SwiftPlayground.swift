8748a4ff3149d41af1725c0c5070acf960fb2837
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-03-17 23:54:54 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -14,13 +14,13 @@ struct Purchaser: Identifiable, Codable, FetchableRecord, PersistableRecord {
     var count: Int
     /// The table reserved by the purchaser
     var reservedTable: Int
-    
+
     enum CodingKeys: String, CodingKey {
         case id = "Purchaser ID"
         case name = "Name"
         case count = "Count"
         case reservedTable = "Reserved Table"
-    } 
+    }
 }
 
 struct Order: Identifiable, Codable, FetchableRecord, PersistableRecord {
@@ -31,7 +31,7 @@ struct Order: Identifiable, Codable, FetchableRecord, PersistableRecord {
     /// the name of the item ordered
     var amount: Int
     /// the price of the item ordered
-    
+
     enum CodingKeys: String, CodingKey {
         case id = "Order ID"
         case purchaserID = "Purchaser ID"
@@ -46,14 +46,28 @@ struct Item: Identifiable, Codable, FetchableRecord, PersistableRecord {
     var name: String
     /// the price of the item
     var price: Double
-    
+
     enum CodingKeys: String, CodingKey {
         case id = "Item ID"
         case name = "Name"
-        car price = "Price"
+        case price = "Price"
     }
 }
 
+struct OrderLine: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    /// the Order ID of the order that the order line belongs to
+    var id: Int
+    /// the Item ID of the item ordered in the order line
+    var itemID: Int
+    /// the quantity of the item ordered in the order line
+    var quantity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "Order ID"
+        case itemID = "Item ID"
+        case quantity = "Quantity"
+    }
+}
 
 @main
 struct SwiftPlayground {
@@ -61,10 +75,22 @@ struct SwiftPlayground {
         let dbpath = "Sources/SwiftPlayground/cafe.db"
         do {
             let dbQueue = try DatabaseQueue(path: dbpath)
-            print("database connectoin succesful")
+            print("database connection succesful")
 
             try dbQueue.read { database in
-            try database.dumpSchema()}
+                try database.dumpSchema()
+            }
+            let purchaserID = 1
+
+            try dbQueue.read { db in
+                let purchaser = try Purchaser.fetchOne(db, key: purchaserID)
+                if let purchaser {
+                    print("Found purchaser with ID \(purchaser.id): \(purchaser.name)")
+                } else {
+                    print("No purchaser with id \(purchaserID)")
+                }
+            }
+            
         } catch {
             print(error)
         }
