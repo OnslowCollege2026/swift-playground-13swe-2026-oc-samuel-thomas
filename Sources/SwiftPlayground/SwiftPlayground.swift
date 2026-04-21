aa35aa932f3d85101b50e8fb5518862a70b9328c
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-04-21 22:51:00 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,17 @@ import Foundation
 @main
 struct SwiftPlayground {
     static func main() {
-        
+        let list = [3, 7, 8, 18]
+        let sum = list.reduce(0) { $0 + $1 }
+        print(sum)
+        let oddNumbers = list.filter { $0 % 2 != 0 }
+        print(oddNumbers)
+        let highestValue = list.reduce(0) {Swift.max($0, $1)}
+        print(highestValue)
+        let below15 = list.filter {$0 < 15}
+        print(below15)
+        let roundedUp = list.map 
+        print(roundedUp)
+
     }
 }
