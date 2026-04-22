68d4f1ede4a2a7b1f7e1ac38d21e25e8b9b4eb64
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-04-22 00:42:11 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,38 +1,11 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// Created by Samuel Thomas
+// Created on 22-04-2026
 
 
-func totalCost(prices: [Double]) -> Double {
-    var totalCost: Double = 0.0
-    for price in prices {
-        totalCost += price
-        }
-    return totalCost
-}
 
-func isOverBudget(total: Double, budget: Double) -> Bool{
-    return total > budget
-}  
-
-func averageCost(prices: [Double]) -> Double {
-    return totalCost(prices: prices) / Double(prices.count)
-}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
-        var day:Int = 1
-        for price in lunches {
-            print("Day \(day): $\(price)")
-            day += 1
-        }
-        print(totalCost(prices: lunches))
-        print(averageCost(prices: lunches))
-        if isOverBudget(total: totalCost(prices: lunches), budget: 35.00) {
-            print("You are over budget!")
-        } else {
-            print("You are within budget.")
-        }
     }
 }
