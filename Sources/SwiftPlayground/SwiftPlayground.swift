2761cf14a9fe8ecd7ca9e75d59a5f8911168f920
oc-samuel-thomas <133182076+oc-samuel-thomas@users.noreply.github.com>
2026-04-30 23:13:12 +0000

    save

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,13 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-// remember the Int? is unreliable do something about that
+// remember the Int? possibly unreliable and might need to do something about that
 // remember the unavailable and available book functions, could be seen as repetitive
 // remember to change the test types in testing
 // maybe add a required amount of numbers for the phone number
 // think about adding a unique factor to the email and phone, possibly not though as kids may use parents phone or email, maybe just make email unique and phone not unique?
 // search book/borrower and edit book/borrower should maybe be combined idk tho.
 // for search borrower be able to search by name email or phone or id similar thing with search book
+// remember to add documentation throughout
 
 
 import Foundation
